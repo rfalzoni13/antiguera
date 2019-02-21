e8303375876704a7e81af3e6320946bb8749df7f
Renato Lopes Falzoni <renato.falzoni@it2b.com.br>
2019-02-21 00:47:00 -0300

    Adição de AutoMapper na camada Administrador

---
+++ Antiguera.Administrador/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace Antiguera.Administrador.AutoMapper
+{
+    public class AutoMapperConfig
+    {
+        public static void RegisterMappings()
+        {
+            Mapper.Initialize(x =>
+            {
+                x.AddProfile<ModelToDTOMappingProfile>();
+                x.AddProfile<DTOToModelMappingProfile>();
+            });
+        }
+    }
+}
